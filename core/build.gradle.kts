3cc06ed335b8147c760dd1bfe6dc57a114914387
andylamax <andylamax@programmer.net>
2023-05-02 15:26:52 +0300

    Fixed identifier-legal-sdk-client-core to build

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -18,7 +18,7 @@ kotlin {
     val tvOsTargets = if (Targeting.OSX) tvOsTargets() else listOf()
     val macOsTargets = if (Targeting.OSX) macOsTargets() else listOf()
     val watchOsTargets = if (Targeting.OSX) watchOsTargets() else listOf()
-    val osxTargets = if (Targeting.OSX) (iosTargets + tvOsTargets + macOsTargets + watchOsTargets) else listOf()
+    val osxTargets = iosTargets + tvOsTargets + macOsTargets + watchOsTargets
     val ndkTargets = if (Targeting.NDK) ndkTargets() else listOf()
     val linuxTargets = if (Targeting.LINUX) linuxTargets() else listOf()
     val mingwTargets = if (Targeting.MINGW) mingwTargets() else listOf()
