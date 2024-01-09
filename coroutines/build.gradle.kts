1511f7ee9c00e4838d4379a7f7343214417e17aa
andylamax <andylamax@programmer.net>
2024-01-09 04:26:49 +0300

    Added mustRunAfter to enable CC to kick in

--- coroutines/build.gradle.kts
+++ coroutines/build.gradle.kts
@@ -24,3 +24,17 @@ kotlin {
         }
     }
 }
+
+rootProject.the<NodeJsRootExtension>().apply {
+    nodeVersion = npm.versions.node.version.get()
+    nodeDownloadBaseUrl = npm.versions.node.url.get()
+}
+
+rootProject.tasks.withType<KotlinNpmInstallTask>().configureEach {
+    args.add("--ignore-engines")
+}
+
+tasks.named("wasmJsTestTestDevelopmentExecutableCompileSync").configure {
+    mustRunAfter(tasks.named("jsBrowserTest"))
+    mustRunAfter(tasks.named("jsNodeTest"))
+}
